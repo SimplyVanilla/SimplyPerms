80ba635bee656cd63f80fd9ac549e0023b206db8
Fulminazzo <fulminazzo21@gmail.com>
2024-03-02 18:48:24 +0100

    Created GroupYAMLParser

---
+++ src/main/java/it/fulminazzo/simplyperms/groups/GroupYAMLParser.java
@@ -0,0 +1,40 @@
+package it.fulminazzo.simplyperms.groups;
+
+import com.velocitypowered.api.permission.Tristate;
+import it.fulminazzo.fulmicollection.interfaces.functions.BiFunctionException;
+import it.fulminazzo.fulmicollection.interfaces.functions.TriConsumer;
+import it.fulminazzo.yamlparser.configuration.ConfigurationSection;
+import it.fulminazzo.yamlparser.configuration.IConfiguration;
+import it.fulminazzo.yamlparser.parsers.YAMLParser;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class GroupYAMLParser extends YAMLParser<Group> {
+
+    public GroupYAMLParser(final @NotNull Class<Group> groupClass) {
+        super(groupClass);
+    }
+
+    @Override
+    protected BiFunctionException<@NotNull IConfiguration, @NotNull String, @Nullable Group> getLoader() {
+        return (c, s) -> {
+            ConfigurationSection section = c.getConfigurationSection(s);
+            if (section == null) return null;
+            return new Group(section);
+        };
+    }
+
+    @Override
+    protected TriConsumer<@NotNull IConfiguration, @NotNull String, @Nullable Group> getDumper() {
+        return (c, s, g) -> {
+            c.set(s, null);
+            if (g == null) return;
+            s = g.getName();
+            ConfigurationSection section = c.createSection(s);
+            for (final String perm : g.permissions.keySet()) {
+                Tristate tristate = g.permissions.get(perm);
+                section.set(perm, tristate.equals(Tristate.TRUE));
+            }
+        };
+    }
+}
