1b9cb1073e2a1d4d66dd6342d5aa3ce07c4bd506
Fulminazzo <fulminazzo21@gmail.com>
2024-03-02 21:19:38 +0100

    Fixed UserYAMLParser for correctly loading users

--- src/main/java/it/fulminazzo/simplyperms/users/UserYAMLParser.java
+++ src/main/java/it/fulminazzo/simplyperms/users/UserYAMLParser.java
@@ -1,10 +1,38 @@
 package it.fulminazzo.simplyperms.users;
 
-import it.fulminazzo.yamlparser.parsers.CallableYAMLParser;
+import it.fulminazzo.fulmicollection.interfaces.functions.BiFunctionException;
+import it.fulminazzo.fulmicollection.interfaces.functions.TriConsumer;
+import it.fulminazzo.yamlparser.configuration.IConfiguration;
+import it.fulminazzo.yamlparser.parsers.YAMLParser;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class UserYAMLParser extends CallableYAMLParser<User> {
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserYAMLParser extends YAMLParser<User> {
 
     public UserYAMLParser() {
-        super(User.class, c -> new User(c.getName()));
+        super(User.class);
+    }
+
+    @Override
+    protected BiFunctionException<@NotNull IConfiguration, @NotNull String, @Nullable User> getLoader() {
+        return (c, s) -> {
+            User user = new User(s);
+            Set<String> tmp = c.get(s, HashSet.class);
+            if (tmp != null) user.groups.addAll(tmp);
+            return user;
+        };
+    }
+
+    @Override
+    protected TriConsumer<@NotNull IConfiguration, @NotNull String, @Nullable User> getDumper() {
+        return (c, s, u) -> {
+            c.set(s, null);
+            if (u == null) return;
+            s = u.getUniqueId().toString();
+            c.set(s, u.groups);
+        };
     }
 }
