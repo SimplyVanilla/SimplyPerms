a66f2934a43e6e521dbd33060b67e6671a6fd993
Fulminazzo <fulminazzo21@gmail.com>
2024-03-02 19:27:06 +0100

    Created GroupUtils#loadGroups

---
+++ src/main/java/it/fulminazzo/simplyperms/utils/GroupUtils.java
@@ -0,0 +1,23 @@
+package it.fulminazzo.simplyperms.utils;
+
+import it.fulminazzo.simplyperms.groups.Group;
+import it.fulminazzo.yamlparser.configuration.ConfigurationSection;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class GroupUtils {
+
+    public static Set<Group> loadGroups(final FileConfiguration configuration) {
+        final Set<Group> groups = new LinkedHashSet<>();
+        final ConfigurationSection groupsSection = configuration.getConfigurationSection("groups");
+        if (groupsSection != null) {
+            for (final String key : groupsSection.getKeys()) {
+                ConfigurationSection groupSection = groupsSection.getConfigurationSection(key);
+                if (groupSection != null) groups.add(new Group(groupSection));
+            }
+        }
+        return groups;
+    }
+}
