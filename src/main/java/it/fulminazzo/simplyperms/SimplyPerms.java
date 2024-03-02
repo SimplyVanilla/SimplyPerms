00103fe8efc09c0b8ead4e54c7dacdf54c23b8a9
Fulminazzo <fulminazzo21@gmail.com>
2024-03-02 18:29:20 +0100

    Added injected constructor

--- src/main/java/it/fulminazzo/simplyperms/SimplyPerms.java
+++ src/main/java/it/fulminazzo/simplyperms/SimplyPerms.java
@@ -4,7 +4,13 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import lombok.Getter;
 import org.slf4j.Logger;
+
+import java.io.File;
+import java.nio.file.Path;
 
 @Plugin(
         id = "simplyperms",
@@ -13,12 +19,24 @@ import org.slf4j.Logger;
         description = "A simple plugin to manage permissions in your server via config.yml. Supports only Velocity.",
         authors = {"Fulminazzo"}
 )
+@Getter
 public class SimplyPerms {
+    @Getter
+    private static SimplyPerms plugin;
+    private final ProxyServer proxyServer;
+    private final Logger logger;
+    private final File dataDirectory;
 
     @Inject
-    private Logger logger;
+    public SimplyPerms(ProxyServer proxyServer, Logger logger, @DataDirectory Path dataDirectory) {
+        plugin = this;
+        this.proxyServer = proxyServer;
+        this.logger = logger;
+        this.dataDirectory = dataDirectory.toFile();
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+
     }
 }
