e77e44eac787fd0de065a591a8bc5264bf41e386
Fulminazzo <fulminazzo21@gmail.com>
2024-03-02 19:22:59 +0100

    Created GroupedTest for test that require groups

---
+++ src/test/java/it/fulminazzo/simplyperms/GroupedTest.java
@@ -0,0 +1,41 @@
+package it.fulminazzo.simplyperms;
+
+import it.fulminazzo.simplyperms.groups.Group;
+import it.fulminazzo.yamlparser.configuration.ConfigurationSection;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * A test that loads and unloads groups per each iteration.
+ */
+public abstract class GroupedTest {
+
+    @BeforeEach
+    void setUp() {
+        loadGroups();
+        int size = Group.getGroups().size();
+        assertEquals(3, size, String.format("Expected 3 groups but got %s", size));
+    }
+
+    @AfterEach
+    void tearDown() {
+        Group.clearGroups();
+        int size = Group.getGroups().size();
+        assertEquals(0, size, String.format("Expected 0 groups but got %s", size));
+    }
+
+    public static void loadGroups() {
+        FileConfiguration configuration = new FileConfiguration("build/resources/test/config.yml");
+
+        final ConfigurationSection groupsSection = configuration.getConfigurationSection("groups");
+        if (groupsSection != null) {
+            for (final String key : groupsSection.getKeys()) {
+                ConfigurationSection groupSection = groupsSection.getConfigurationSection(key);
+                if (groupSection != null) new Group(groupSection);
+            }
+        }
+    }
+}
