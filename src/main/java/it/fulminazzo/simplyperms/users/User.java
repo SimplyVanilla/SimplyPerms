3e62aa91771c32f7680d5c63490c4f204de78c8d
Fulminazzo <fulminazzo21@gmail.com>
2024-03-02 19:05:36 +0100

    Created User object

---
+++ src/main/java/it/fulminazzo/simplyperms/users/User.java
@@ -0,0 +1,63 @@
+package it.fulminazzo.simplyperms.users;
+
+import com.velocitypowered.api.permission.Tristate;
+import it.fulminazzo.fulmicollection.objects.Printable;
+import it.fulminazzo.simplyperms.PermissionHolder;
+import it.fulminazzo.simplyperms.groups.Group;
+import lombok.Getter;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Getter
+public class User extends Printable implements PermissionHolder {
+    private static final List<User> USERS = new LinkedList<>();
+    private final UUID uniqueId;
+    private Set<String> groups;
+
+    public User(final String rawId) {
+        try {
+            this.uniqueId = UUID.fromString(rawId);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(String.format("'%s' is not a valid UUID", rawId));
+        }
+
+        USERS.add(this);
+    }
+
+    @Override
+    public boolean hasPermission(final String permission) {
+        return getGroups().stream()
+                .sorted(Collections.reverseOrder())
+                .map(g -> g.getPermissionState(permission))
+                .filter(s -> s != Tristate.UNDEFINED)
+                .findFirst().orElse(Tristate.FALSE)
+                .equals(Tristate.UNDEFINED);
+    }
+
+    @Override
+    public Set<String> getPermissions() {
+        return getGroups().stream().flatMap(g -> g.getPermissions().stream()).collect(Collectors.toSet());
+    }
+
+    public Set<Group> getGroups() {
+        return this.groups.stream()
+                .map(Group::getGroup)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+    }
+
+    public static User getUser(final UUID uniqueId) {
+        return uniqueId == null ? null : getUsers().stream()
+                .filter(g -> g.getUniqueId().equals(uniqueId))
+                .findFirst().orElse(null);
+    }
+
+    public static List<User> getUsers() {
+        return new LinkedList<>(USERS);
+    }
+
+    public static void clearUsers() {
+        USERS.clear();
+    }
+}
