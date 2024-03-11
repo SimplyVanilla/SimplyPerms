fe6caaed700655612f00ea5e00073b6b573d945f
Fulminazzo <fulminazzo21@gmail.com>
2024-03-11 13:07:29 +0100

    Created UsersManager to keep track of loaded users

---
+++ src/main/java/net/simplyvanilla/simplyperms/users/UsersManager.java
@@ -0,0 +1,29 @@
+package net.simplyvanilla.simplyperms.users;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+public class UsersManager {
+    private final List<User> users;
+
+    public UsersManager() {
+        this.users = new LinkedList<>();
+    }
+
+    public User getUser(final UUID uniqueId) {
+        return uniqueId == null ? null : getUsers().stream()
+            .filter(g -> g.getUniqueId().equals(uniqueId))
+            .findFirst().orElseGet(() -> new User(uniqueId.toString()));
+    }
+
+    public List<User> getUsers() {
+        this.users.removeIf(Objects::isNull);
+        return new LinkedList<>(this.users);
+    }
+
+    public void clearUsers() {
+        this.users.clear();
+    }
+}
